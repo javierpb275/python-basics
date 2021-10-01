343bf0e2370a02ca64dc0d602ae643975ab16f1c
javierpb275 <javierpb275@gmail.com>
2021-10-01 19:39:12 +0200

    define variable, input() and print() methods

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+#grabs the value typed in the terminal and store it in the variable asigned:
+name = input("what's your name? ")
+#print message in the terminal:
+print("Hello " + name)
