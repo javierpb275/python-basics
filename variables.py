a5eac1075a9e6b2c70472ac4fc047ded1a2a23e7
javierpb275 <javierpb275@gmail.com>
2021-10-02 20:26:14 +0200

    variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+user_id = 1  # snake_case
+_user_name = 'pepe'  # _ at the beginning meas private variable
+print(user_id)
+print(_user_name)
+
+# constants
+PI = 3.14
+
+# dunder variables:
+# __builtins__
+
+a, b, c = 1, 2, 3
+
+print(a, b, c)
