5f970707a27d872e22d3b40d6dc63b7b98a6af95
javierpb275 <javierpb275@gmail.com>
2021-10-02 19:13:29 +0200

    data types intro

---
+++ dataTypes.py
@@ -0,0 +1,15 @@
+# data types:
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types: Special packages and modules that we can use from libraries.
+
+# none: nothing. absence of value.
