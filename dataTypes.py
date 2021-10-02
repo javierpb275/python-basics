6f8e7db2ba97e5a5042868c34483362e99dacf65
javierpb275 <javierpb275@gmail.com>
2021-10-02 20:41:45 +0200

    moved numbers explanation to numbers.oy file

--- dataTypes.py
+++ dataTypes.py
@@ -1,53 +1,10 @@
 # data types:
 
 # NUMBERS:
-
-""" # integer:
-# int
-print(type(2+4))
-print(2-4)
-print(2*4)
-print(2/4)#0.5: float
-
-# float (need more memory)
-print(type(0.0003))
-print(0.15+0.23)
-print(0.5+0.5)# 1.0: still a float number
-
-#power of 3:
-print(2**3)
-#returns integer rounded down:
-print(5//4)#1
-#returns remainder of division:
-print(6%4)#1 
-
-# math functions:
-print(round(3.5))  # rounds the number (4)
-# returns absolute value of the number (non negative numbers)
-print(abs(-20))  # 20
-
-
-# operator precedence:
-print((20-3) + 2 ** 2)
-# 1-> (); 2-> **; 3-> *; 4-> * /; 5-> + -
-
-
-#complex
-complex
-#bin: returns the binary version
-print(bin(5))#0b101 (101)
-print(int('0b101', 2))#5
-"""
-
-#augmented assignment operator:
-some_value = 5
-
-some_value /= 2
-print(some_value)
-
-some_value += 2
-some_value -= 2
-some_value *= 2
+#int
+my_integer = 1
+#float
+my_float = 2.5
 
 """ 
 bool
