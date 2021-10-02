392580d15e56ba11c68748315030ba283143e002
javierpb275 <javierpb275@gmail.com>
2021-10-02 19:45:26 +0200

    type numbers: int and float

--- dataTypes.py
+++ dataTypes.py
@@ -1,12 +1,33 @@
 # data types:
-int
-float
+
+# NUMBERS:
+
+# integer:
+# int
+print(type(2+4))
+print(2-4)
+print(2*4)
+print(2/4)#0.5: float
+
+# float (need more memory)
+print(type(0.0003))
+print(0.15+0.23)
+print(0.5+0.5)# 1.0: still a float number
+
+#power of 3:
+print(2**3)
+#returns integer rounded down:
+print(5//4)#1
+#returns remainder of division:
+print(6%4)#1
+
+""" 
 bool
 str
 list
 tuple
 set
-dict
+dict """
 
 # Classes -> custom types
 
