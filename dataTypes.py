dd7587d0e716031ac42094081796685eaefcef9e
javierpb275 <javierpb275@gmail.com>
2021-10-04 18:11:15 +0200

    string indexes

--- dataTypes.py
+++ dataTypes.py
@@ -33,7 +33,7 @@ weather4 = "It's \"kind of\" sunny"
 weather5 = "\t It's \"kind of\" sunny\n have a nice day"
 print(weather3)
 print(weather4)
-print(weather5) """
+print(weather5) 
 
 # formatted strings
 name = 'pepe'
@@ -41,6 +41,20 @@ age = 30
 print(f'Hi, my name is {name} and I\'m {age} ')
 print('Hi, my name is {1} and I\'m {0} '.format(name, age))
 print('Hi, my name is {new_name} and I\'m {new_age} '.format(new_name='paco', new_age=25))
+"""
+# string indexes
+abc = 'abcdefghijklmnopqrstuvwxyz'
+print(abc[0])  # a
+
+
+# [start:stop:stepover]
+the_numbers = '0123456789'
+print(the_numbers[0:5])  # 01234
+print(the_numbers[0:5:2])  # 024
+print(the_numbers[5:])  # 56789
+print(the_numbers[:5])  # 01234
+print(the_numbers[::2])  # 02468
+print(the_numbers[::-1])  # 9876543210
 
 """ 
 bool
