a164ca3abad5ceb89fa63916f77df013ebd43990
javierpb275 <javierpb275@gmail.com>
2021-10-05 18:29:34 +0200

    moved code of built-in functions and methods

--- functions-and-methods/built-in_functions_and_methods.py
+++ functions-and-methods/built-in_functions_and_methods.py
@@ -1,9 +1,15 @@
 # BUILT-IN FUNCTIONS:
 
+#grabs the value typed in the terminal and store it in the variable asigned:
+name = input("what's your name? ")
+#print message in the terminal:
+print("Hello " + name)
+
 # len(): length
 print(len('012345'))  # 6
 name = 'javier'
 print(name[0:len(name)])  # javier
+
 
 # METHODS:
 
@@ -25,4 +31,4 @@ print(quote.find('m'))  # -1
 # replace():
 print(quote.replace('be', 'me'))
 
-print(quote)  # to be or not to be. strings are immutable. we can overwrite them, we either create them or destroy them
+print(quote)  # to be or not to be. Strings are immutable. We can overwrite them. We either create them or destroy them
--- main.py
+++
@@ -1,4 +0,0 @@
-#grabs the value typed in the terminal and store it in the variable asigned:
-name = input("what's your name? ")
-#print message in the terminal:
-print("Hello " + name)
